45a1682ff36894aa2663fa1b6564450164ebe545
Chris Bassey <chrixsbassey@gmail.com>
2023-05-04 11:09:02 +0200

    code refactoring of invitation controller for oop

--- source/publisher/invitationSender.ts
+++ source/publisher/invitationSender.ts
@@ -1,22 +1,3 @@
-// import * as amqp from 'amqplib';
-
-// const createMQProducer = async (rabbitMQURL: string, queueName: string) => {
-//   console.log('Connecting to RabbitMQ...');
-//   const connection = await amqp.connect(rabbitMQURL);
-//   const channel = await connection.createChannel();
-
-//   console.log('Connected to RabbitMQ');
-
-//   const producer = (msg: string) => {
-//     console.log('Produce message to RabbitMQ...');
-//     channel.sendToQueue(queueName, Buffer.from(msg));
-//   };
-
-//   return producer;
-// };
-
-// export default createMQProducer;
-
 import { Channel, Connection, ConsumeMessage } from "amqplib";
 import * as client from "amqplib";
 
@@ -29,7 +10,6 @@ class MQPublisher {
   constructor(rabbitMQURL: string, queueName: string) {
     this.queueName = queueName;
     this.rabbitMQURL = rabbitMQURL;
-    // this.connect(rabbitMQURL);
   }
 
   async start(): Promise<void> {
@@ -37,28 +17,13 @@ class MQPublisher {
     this.connection = await client.connect(this.rabbitMQURL);
     this.channel = await this.connection.createChannel();
     console.log('Connected to RabbitMQ');
-    // await this.channel.assertQueue(this.queueName);
-    // await this.channel.consume(this.queueName, this.onMessageReceived.bind(this));
   }
-
-  // private async connect(rabbitMQURL: string) {
-  //   console.log('Connecting to RabbitMQ...');
-  //   this.connection = await amqp.connect(rabbitMQURL);
-  //   this.channel = await this.connection.createChannel();
-  //   console.log('Connected to RabbitMQ');
-  // }
 
   public async publish(msg: string) {
     console.log('Sending message to RabbitMQ...');
     this.channel?.sendToQueue(this.queueName, Buffer.from(msg));
   }
 
-  // public async close() {
-  //   console.log('Closing connection to RabbitMQ...');
-  //   await this.channel.close();
-  //   await this.connection.close();
-  //   console.log('Connection closed');
-  // }
   async stop(): Promise<void> {
     console.log('Closing connection to RabbitMQ...');
     await this.channel?.close();
--- source/routes/invitation.routes.ts
+++ source/routes/invitation.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import {
-  computeInvites,
-} from "../controllers/invitation.controller";
-const router: Router = Router();
+// import {
+//   InvitesController,
+// } from "../controllers/invitation.controller";
 
-router.route("/compute-invites").post(computeInvites);
+import InvitesController from "../controllers/invitation.controller";
+const router: Router = Router();
+const rabbitMQURL: string = "amqp://username:password@192.168.33.17:5672";
+const eventQueue: string = "customerInvites";
+const invitationController = new InvitesController(rabbitMQURL, eventQueue);
+router.route("/compute-invites").post(invitationController.computeInvites);
 
 export default router;
