4d3ff97d4599dc8546a83ca84b81ad490f30bba8
Chris Bassey <chrixsbassey@gmail.com>
2023-05-07 10:44:22 +0200

    exporting environment variables

--- source/index.ts
+++ source/index.ts
@@ -5,16 +5,15 @@ import * as express from 'express';
 import invitationRouter from "./routes/invitation.routes";
 import MQConsumer from './subscriber/invitationSubscriber';
 
+const rabbitMQURL: string = process.env.RABBITMQ_URL?.toString() || "amqp://username:password@192.168.33.17:5672";
+const eventQueue: string = "customerInvites";
 
 const main = async (): Promise<void> => {
-
   const app = express();
-
   app.use(express.json());
 
   const port = process.env.PORT;
-  const rabbitMQURL: string = process.env.RABBITMQ_URL?.toString() || "amqp://username:password@192.168.33.17:5672";
-  const eventQueue: string = "customerInvites";
+
   const consumer = new MQConsumer(rabbitMQURL, eventQueue);
   await consumer.start();
 
@@ -22,8 +21,7 @@ const main = async (): Promise<void> => {
   app.get('/', (req, res) => {
     res.send('Hello!');
   });
-  app.use("/api", invitationRouter);
-
+  app.use("/api", invitationRouter(rabbitMQURL, eventQueue));
 
   app.listen(port, (): void => {
     console.log(`Server is running on port ${port}`);
@@ -33,3 +31,5 @@ const main = async (): Promise<void> => {
 main().catch((err) => {
   console.log(err);
 });
+
+export { rabbitMQURL, eventQueue };
--- source/routes/invitation.routes.ts
+++ source/routes/invitation.routes.ts
@@ -1,11 +1,26 @@
+// import { Router } from "express";
+// import InvitesController from "../controllers/invitation.controller";
+
+
+// const router: Router = Router();
+// const rabbitMQURL: string = "amqp://username:password@192.168.33.17:5672";
+// const eventQueue: string = "customerInvites";
+// const invitationController = new InvitesController(rabbitMQURL, eventQueue);
+// router.route("/compute-invites").post(invitationController.computeInvites);
+
+// export default router;
+
 import { Router } from "express";
 import InvitesController from "../controllers/invitation.controller";
 
-
 const router: Router = Router();
-const rabbitMQURL: string = "amqp://username:password@192.168.33.17:5672";
-const eventQueue: string = "customerInvites";
-const invitationController = new InvitesController(rabbitMQURL, eventQueue);
-router.route("/compute-invites").post(invitationController.computeInvites);
 
-export default router;
+const invitationRouter = (rabbitMQURL: string, eventQueue: string): Router => {
+  const invitationController = new InvitesController(rabbitMQURL, eventQueue);
+
+  router.route("/compute-invites").post(invitationController.computeInvites);
+
+  return router;
+};
+
+export default invitationRouter;
